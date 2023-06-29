786c24efe5c73fc75883c9bef01a798170f7231e
Andrés Weitzel <andres96energy@hotmail.com>
2023-06-29 12:53:53 -0300

    Updated DDL

--- Scripts/01_db_dispositivos_electronicos_DDL.sql
+++ Scripts/01_db_dispositivos_electronicos_DDL.sql
@@ -80,3 +80,77 @@ check (precio > 0 );
 
 -- ---------------------------------------------------------------------------
 
+-- ======= TABLA COMPONENTES_DETALLES ===========
+
+create table componentes_detalles(
+	
+id 	int primary key,
+id_componente int not null,
+hoja_de_datos varchar(3000)	not null, --link datasheet
+longitud varchar(30),-- 69.0 mm
+ancho varchar(30),-- 56.7 mm
+peso varchar(30),-- 19.4 gramos
+material varchar(50),-- silicio,acero, plastico, etc
+voltaje_recomendado	varchar(30), -- 5 voltios
+voltaje_min_entrada	varchar(30),-- 7  voltios (recomendado)
+voltaje_max_entrada	varchar(30)-- 12  voltios (recomendado)
+
+);
+
+-- ======= Restricciones Tabla componentes_detalles ===========
+
+-- UNIQUE ID_COMPONENTE
+alter table componentes_detalles
+add constraint UNIQUE_componentes_detalles_id_componente
+unique (id_componente);
+
+
+-- FK ID_COMPONENTE
+alter table componentes_detalles 
+add constraint FK_componentes_detalles_id_componente
+foreign key(id_componente)
+references componentes(id)
+on delete cascade;
+
+
+-- ---------------------------------------------------------------------------
+
+-- ---------------------------------------------------------------------------
+
+-- ======= TABLA TRANSISTORES_BIPOLARES ===========
+
+create table transistores_bipolares(
+	
+id int primary key,
+id_componente int not null,
+tipo varchar(10) not null, -- NPN, PNP
+--Desempeño
+voltaje_colec_emis varchar(30), ---30V, 5.5V
+voltaje_colec_base varchar(30), ---30V, 5.5V
+voltaje_emis_base varchar(30), ---30V, 5.5V
+voltaje_colec_emis_sat varchar(30), ---30V, 5.5V
+corriente_colec varchar(30), ---1.5A
+ganancia_hfe varchar(30), ---1.5A
+--Caract. Térmicas			
+disip_max varchar(30), ---0.645W
+temp_juntura varchar(50)    ----55a+155 °C
+
+);
+
+-- ======= Restricciones Tabla transistores_bipolares ===========
+
+-- UNIQUE ID_COMPONENTE
+alter table transistores_bipolares
+add constraint UNIQUE_transistores_bipolares_id_componente
+unique (id_componente);
+
+
+-- FK ID_COMPONENTE
+alter table transistores_bipolares 
+add constraint FK_transistores_bipolares_id_componente
+foreign key(id_componente)
+references componentes(id)
+on delete cascade;
+
+
+
