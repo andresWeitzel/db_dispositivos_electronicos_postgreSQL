d88e1931b75d9142cfdc32d3e7270300cf778ee3
Andrés Weitzel <andres96energy@hotmail.com>
2023-07-23 12:29:58 -0300

    Added update cases for DML UPDATES

--- Scripts/03_microdb_ml_productos_DML_UPDATE.sql
+++ Scripts/03_microdb_ml_productos_DML_UPDATE.sql
@@ -35,6 +35,10 @@ and comp.id = comp_det.id_componente
 and comp_det.hoja_de_datos = 'https://ar.mouser.com/c/ds/passive-components/capacitors/aluminum-electrolytic-capacitors/aluminum-electrolytic-capacitors-screw-terminal/'
 and comp.fabricante = 'RUBYCON');
 
+-- Actualizamos nombre de fabricante
+update componentes  set fabricante = replace(fabricante, 'Renesas Electronics' , 'RENESAS ELECTRONICS');
+
+
 
 -- ---------------------------------------------------------------------------
 -- ---------------------------------------------------------------------------
@@ -60,3 +64,59 @@ from componentes as comp
 where (comp_det.id_componente = comp.id 
 and comp.categoria = 'Resistores de Alta Frecuencia'
 and comp_det.hoja_de_datos = 'https://www.vishay.com/docs/61038/mif.pdf');
+
+
+select * from componentes_detalles as comp_det 
+inner join componentes as comp
+on comp_det.id_componente  = comp.id
+where categoria = 'Placas Arduino' and nro_pieza='UTT-ARDUINO-NANO';
+
+
+-- Actualizamos material y voltajes para placas arduino según nro de pieza
+update componentes_detalles as comp_det set material = 'Aluminio aleación'
+, voltaje_recomendado='3 - 3.3 vdc', voltaje_min_entrada = '3 vdc'
+, voltaje_max_entrada = '3.3 vdc'
+from componentes as comp
+where comp_det.id_componente  = comp.id 
+and categoria = 'Placas Arduino' and nro_pieza='UTT-ARDUINO-NANO';
+
+-- Reemplazamos el caracter 'a' por el  '-' del campo VOLTAJE_RECOMENDADO
+update componentes_detalles set voltaje_recomendado = replace(voltaje_recomendado, 'a' , '-');
+
+-- Reemplazamos vdc por VDC
+update componentes_detalles set voltaje_recomendado = replace(voltaje_recomendado, 'vdc' , 'VDC');
+update componentes_detalles set voltaje_min_entrada = replace(voltaje_min_entrada, 'vdc' , 'VDC');
+update componentes_detalles set voltaje_max_entrada = replace(voltaje_max_entrada, 'vdc' , 'VDC');
+
+
+
+-- ---------------------------------------------------------------------------
+-- ---------------------------------------------------------------------------
+
+
+-- ==============================================
+-- ======= TABLA TRANSISTORES_BIPOLARES==========
+-- ==============================================
+
+select * from componentes;
+select * from componentes_detalles;
+select * from transistores_bipolares;
+select * from information_schema.columns where table_name='transistores_bipolares';
+
+
+select * from transistores_bipolares as trans_bip 
+inner join componentes_detalles as comp_det
+on trans_bip.id_componente = comp_det.id_componente
+inner join componentes as comp
+on comp_det.id_componente = comp.id
+where comp.categoria = 'Transistores BJT' 
+and comp_det.hoja_de_datos = 'https://category.alldatasheet.com/index.jsp?components=TRANSISTOR'
+and trans_bip.tipo = 'NPN';
+
+/*
+-- Actualizamos voltajes según tipo de transistor
+update transistores_bipolares as trans_bip
+set 
+*/
+
+
